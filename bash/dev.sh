0c021102b28236c64f44b4aec1594fd98534811b
miyabin66 <freezemark5166@gmail.com>
2022-01-14 18:15:37 +0900

    2つ以上帰ってきた場合の処理

--- bash/dev.sh
+++ bash/dev.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
-next dev --hostname $IP
+IPS=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+# 2つ以上帰ってきた場合は先頭のIPを使用する
+IP=(${IPS// / })
+next dev --hostname ${IP[0]}
