3d2926b3fd6d27d23c55fedc077577841bea025c
miyabin66 <freezemark5166@gmail.com>
2022-01-16 23:07:57 +0900

    リファクタリング

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -25,20 +25,28 @@ const DefaultLayout = ({ children, ogImage, ogUrl, ogDescription }: Prop) => {
     <>
       <Head>
         <title>{meta.title}</title>
+
         <meta
           name="description"
           content={ogDescription ? ogDescription : meta.description}
         />
         <meta name="viewport" content={meta.viewport} />
         <meta name="keywords" content={meta.keywords} />
-        <meta name="application-name" content={meta.applicationName} />
-        <meta
-          name="apple-mobile-web-app-title"
-          content={meta.applicationName}
-        />
+        {meta.applicationName && (
+          <>
+            <meta name="application-name" content={meta.applicationName} />
+            <meta
+              name="apple-mobile-web-app-title"
+              content={meta.applicationName}
+            />
+          </>
+        )}
+
         <link rel="icon" type={meta.faviconType} href={meta.faviconPath} />
         <link rel="canonical" href={meta.url} />
-        <link rel="apple-touch-icon" href={meta.appleTouchIconPath} />
+        {meta.appleTouchIconPath && (
+          <link rel="apple-touch-icon" href={meta.appleTouchIconPath} />
+        )}
 
         <meta property="og:url" content={ogUrl ? meta.url + ogUrl : meta.url} />
         <meta
