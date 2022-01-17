fc3e304539c8a3a7dcf213f27cd6335dc3a83df1
miyabin66 <freezemark5166@gmail.com>
2022-01-17 18:58:44 +0900

    localstorageのクラスも入れておく

---
+++ persistence/index.ts
@@ -0,0 +1,7 @@
+// import Persistence from '~/utils/class/persistence'
+
+const indexPersistences = {
+  // localstorage: new Persistence<string>('index.localstorage'),
+}
+
+export default indexPersistences
---
+++ utils/class/persistence.ts
@@ -0,0 +1,48 @@
+export default class Persistence<T> {
+  resource: string
+
+  constructor(resource: string) {
+    this.resource = resource
+  }
+
+  private _type(ctor: { new (): T }) {
+    return new ctor()
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private _isT(arg: any): arg is T {
+    return typeof arg === typeof this._type
+  }
+
+  public get value(): T {
+    const _v = window.localStorage.getItem(this.resource)
+
+    if (typeof this._type === 'string' && this._isT(_v)) {
+      return _v
+    } else if (typeof this._type === 'boolean') {
+      const b = _v === 'true'
+      if (this._isT(b)) {
+        return b
+      }
+    }
+
+    try {
+      return JSON.parse(`${_v}`)
+    } catch (_e) {
+      throw new Error(`${_e}`)
+    }
+  }
+
+  public set(value: T): void {
+    if (value == null) return
+    window.localStorage.setItem(this.resource, JSON.stringify(value))
+  }
+
+  public remove(): void {
+    window.localStorage.removeItem(this.resource)
+  }
+}
+
+export const clearAll = () => {
+  window.localStorage.clear()
+}
