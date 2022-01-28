519e97b1c088b07299a01a4a35cb8bcf1414f61b
miyabin66 <freezemark5166@gmail.com>
2022-01-28 23:15:01 +0900

    titleとlocation使わなそう

--- utils/gtag.ts
+++ utils/gtag.ts
@@ -6,8 +6,6 @@ declare global {
 
 type SendPageViewProps = {
   path: string
-  title?: string
-  location?: string
 }
 
 type SendGAEventProps = {
@@ -20,30 +18,18 @@ type SendGAEventProps = {
 export const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID
 export const GA_ID = process.env.NEXT_PUBLIC_GA_ID
 
-export const sendGTMPageView = ({
-  path,
-  title = '',
-  location = '',
-}: SendPageViewProps) => {
+export const sendGTMPageView = ({ path }: SendPageViewProps) => {
   window.dataLayer.push({
     page_path: path,
-    page_title: title,
-    page_location: location,
   })
 }
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const sendGAPageView = ({
-  path,
-  title = '',
-  location = '',
-}: SendPageViewProps) => {
+export const sendGAPageView = ({ path }: SendPageViewProps) => {
   if (!GA_ID) return
 
   window.gtag('config', GA_ID, {
     page_path: path,
-    page_title: title,
-    page_location: location,
   })
 }
 
@@ -54,6 +40,8 @@ export const sendGAEvent = ({
   label,
   value,
 }: SendGAEventProps) => {
+  if (!GA_ID) return
+
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
